4b53f4013a0df30ca333d34c43993d694ded599e
Igor Oliveira <igorfoliveira@gmail.com>
2019-03-03 14:17:10 -0300

    Buscando endereços do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -11,28 +13,28 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 20",
-        bairro: "Santa Mônica",
-        cep: "03697101",
-        cidade: {
-          id: "1",
-          nome: "Uberlandia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+        },
+        error =>{
+          if (error.status == 403){
+            this.navCtrl.setRoot('HomePage');
           }
-        }
-      }
-    ]
+        })
+    } else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
