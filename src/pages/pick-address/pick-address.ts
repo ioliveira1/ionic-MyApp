d3daff365b2940407b43926399435d3db4e3bca4
Igor Oliveira <igorfoliveira@gmail.com>
2019-03-09 16:04:29 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -26,7 +26,7 @@ export class PickAddressPage {
 
   nextPage(endereco: EnderecoDTO){
     this.pedido.enderecoDeEntrega = {id: endereco.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
   ionViewDidLoad() {
